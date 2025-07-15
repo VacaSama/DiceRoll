da004e5eb2727781b8623354664a74abd58b3cbc
VacaSama <klc_moore@outlook.com>
2025-07-15 14:58:50 -0700

    Refactor Form1 and add Die class
    Updated Form1 initialization and added Load event handler.
    Corrected comments and formatting in Form1.resx.
    Introduced a new Die class with validation for sides and properties for face value.
    
    Enhance Form1 layout and add Die class
    
    - Updated `InitializeComponent` in `Form1.Designer.cs` to optimize layout with `SuspendLayout` and `ResumeLayout`, added `AutoScaleDimensions`, set `Name` property, and linked `Load` event to `Form1_Load`.
    - Introduced `Form1_Load` method in `Form1.cs`, currently empty.
    - Revised comments in `Form1.resx` for clarity without altering structure.
    - Added new `Die.cs` file defining a `Die` class with properties for sides and face value, including validation in the constructor.

---
+++ DiceRoll/Die.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoll;
+/// <summary>
+/// The Die Class: Represents a die with a specified number of sides.
+/// Often used in games, but can be used for other things and
+/// die have random number generation.
+/// </summary>
+public class Die
+{
+    private static readonly Random rand = new();
+    private const byte MAX_SIDES = 20;
+    /// <summary>
+    /// Gets the number of sides of the die.
+    /// </summary>
+    public byte NumberOfSides { get; init; } // private set; because we don't want external changes
+
+    public byte FaceValue { get; private set; } // this is the value of the die when it is done rolling
+
+    /// <summary>
+    /// Initializes a new instance of the 
+    /// <see cref="Die"/> class with a specified number of sides.
+    /// </summary>
+    /// <param name="numSides"></param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public Die(byte numSides) // byte is used to limit the number of sides possible to 0 - 255
+    {
+        // here we are going to make an exception if the number is euqal to 0 or less than 20 sides 
+        if (numSides == 0 || numSides > MAX_SIDES)
+        {
+            throw new ArgumentOutOfRangeException(nameof(numSides),
+                $"{nameof(numSides)} has to be greater than 0 and less than 225");
+        }
+        NumberOfSides = numSides;
+        // Forces the die to start at a random face value when created.
+        //Roll();
+    }
+}
+// pushing this up to Github to fix the issue with the main branch
--- DiceRoll/Form1.cs
+++ DiceRoll/Form1.cs
@@ -6,5 +6,10 @@ namespace DiceRoll
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
